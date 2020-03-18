4ba84b8feda493c1c86aa10b4f39f93501b72dce
Gritta Weisheit <gritta.weisheit@giz.berlin>
2020-03-18 19:42:18 +0100

    add the removed format again

--- converters.go
+++ converters.go
@@ -86,6 +86,7 @@ func ParseStringToTimestamp(s string, conFailStat *sync.Map) *tspb.Timestamp {
 		"02-Jan-06 03.04.05.000000000 PM MST",
 		"02-Jan-06 03.04.05.000000000 PM -07:00",
 		"20060102030405",
+		"20060102",
 	}
 	for _, importLayout := range importLayouts {
 		newTimestamp, err := time.Parse(importLayout, s)
