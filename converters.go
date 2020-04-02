31ad23781b59ebed593de39ef40a389910ffe050
Christopher <christopher.schulz@student.hpi.de>
2020-04-02 12:49:16 +0200

    [FEAT] Documenting partial refactoring done

--- converters.go
+++ converters.go
@@ -17,12 +17,27 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const LOG_FILE = "./parseError.log"
+// TimeLayoutTrimedTimeZone defines the tz format tZoneLayoutToRemove
+// which shall be removed from a string so that the resulting string has the format tLayoutPostTZRemoval
+// so that string can be parsed as time
+// example TimeLayoutTrimedTimeZone{"2006-01-02", "-07:00"}
+type TimeLayoutTrimedTimeZone struct {
+	tLayoutPostTZRemoval string
+	tZoneLayoutToRemove  string
+}
+
+var importLayoutsWithoutTimeZone = []TimeLayoutTrimedTimeZone{
+	TimeLayoutTrimedTimeZone{"2006-01-02", "-07:00"},
+	TimeLayoutTrimedTimeZone{"15:04:05", "-07:00"},
+}
+
+//LogFile The log filename which gets created in the executing directory
+const LogFile = "./parseError.log"
 
 var errLog *log.Logger
 
 func setupLogFile() {
-	e, err := os.OpenFile(LOG_FILE, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	e, err := os.OpenFile(LogFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 
 	if err != nil {
 		fmt.Printf("error opening file: %v", err)
@@ -30,13 +45,14 @@ func setupLogFile() {
 	}
 	errLog = log.New(e, "", log.Ldate|log.Ltime)
 	errLog.SetOutput(&lumberjack.Logger{
-		Filename:   LOG_FILE,
+		Filename:   LogFile,
 		MaxSize:    1,  // megabytes after which new file is created
 		MaxBackups: 3,  // number of backups
 		MaxAge:     28, //days
 	})
 }
 
+//Parser offers interface to parse strings currently only LoggedParseString
 type Parser interface {
 	storeFailiure()
 	ParseStringToFloat64() float64
@@ -50,13 +66,14 @@ type Parser interface {
 	removeTZ(Pair) (string, error)
 }
 
+//LoggedParseString a pair of string and a *sync.Map which stores parse erros
 type LoggedParseString struct {
 	s           string
 	conFailStat *sync.Map
 }
 
-func (ps LoggedParseString) storeFailure() {
-	storeFailure(ps.s, ps.conFailStat)
+func (lps LoggedParseString) storeFailure() {
+	storeFailure(lps.s, lps.conFailStat)
 }
 
 func storeFailure(unparseable string, conFailStat *sync.Map) {
@@ -69,6 +86,7 @@ func storeFailure(unparseable string, conFailStat *sync.Map) {
 	}
 }
 
+// PrintFailStat prints Statistics of Failed LoggedParseString parsing to console and the LogFile
 func PrintFailStat(conFailStat *sync.Map) {
 	setupLogFile()
 	conFailStat.Range(func(unparseable, counter interface{}) bool {
@@ -79,20 +97,20 @@ func PrintFailStat(conFailStat *sync.Map) {
 	errLog.Printf("-----> End of logging of last Collection and beginning of new Collection if any")
 }
 
-// this converts the J, B notation of bools to go bools
+// ToBool converts the J, B notation of bools to go bools
 func ToBool(s string) bool {
 	if s == "J" {
 		return true
-	} else {
-		return false
 	}
+	return false
 }
 
-func (ps LoggedParseString) ParseStringToFloat64() float64 {
-	return ParseStringToFloat64(ps.s, ps.conFailStat)
+// ParseStringToFloat64 prases the string in LoggedParseString as float64 and logs any failures
+func (lps LoggedParseString) ParseStringToFloat64() float64 {
+	return ParseStringToFloat64(lps.s, lps.conFailStat)
 }
 
-//convinience function to cat errors
+// ParseStringToFloat64 prases the string s as float64 and logs any failures in conFailStat
 func ParseStringToFloat64(s string, conFailStat *sync.Map) float64 {
 	number, err := strconv.ParseFloat(strings.TrimSpace(s), 64)
 	if err != nil {
@@ -102,10 +120,12 @@ func ParseStringToFloat64(s string, conFailStat *sync.Map) float64 {
 	return number
 }
 
-func (ps LoggedParseString) ParseStringToDecimal() decimal.Decimal {
-	return ParseStringToDecimal(ps.s, ps.conFailStat)
+// ParseStringToDecimal prases the string in LoggedParseString as decimal.Decimal and logs any failures
+func (lps LoggedParseString) ParseStringToDecimal() decimal.Decimal {
+	return ParseStringToDecimal(lps.s, lps.conFailStat)
 }
 
+// ParseStringToDecimal prases the string s as decimal.Decimal and logs any failures in conFailStat
 func ParseStringToDecimal(s string, conFailStat *sync.Map) decimal.Decimal {
 	number, err := decimal.NewFromString(s)
 	if err != nil {
@@ -115,10 +135,12 @@ func ParseStringToDecimal(s string, conFailStat *sync.Map) decimal.Decimal {
 	return number
 }
 
-func (ps LoggedParseString) ParseStringToInt64() int64 {
-	return ParseStringToInt64(ps.s, ps.conFailStat)
+// ParseStringToInt64 prases the string in LoggedParseString as int64 and logs any failures
+func (lps LoggedParseString) ParseStringToInt64() int64 {
+	return ParseStringToInt64(lps.s, lps.conFailStat)
 }
 
+// ParseStringToInt64 prases the string s as int64 and logs any failures in conFailStat
 func ParseStringToInt64(s string, conFailStat *sync.Map) int64 {
 	number, err := strconv.ParseInt(strings.TrimSpace(s), 10, 64)
 	if err != nil {
@@ -132,7 +154,7 @@ func ParseStringToInt64(s string, conFailStat *sync.Map) int64 {
 	return number
 }
 
-//this is copied form the tmpmodels
+//ToTimestamp this is copied form the tmpmodels
 func ToTimestamp(t time.Time) *tspb.Timestamp {
 
 	if (t == time.Time{}) {
@@ -143,6 +165,8 @@ func ToTimestamp(t time.Time) *tspb.Timestamp {
 	return ts
 }
 
+// ParseStringToTimestamp prases the string in LoggedParseString as *tspb.Timestamp and logs any failures
+// example layouts
 // 01-APR-19 03.12.00.000000000 PM +02:00
 // 01-APR-19 03.12.00 PM +02:00
 // 01-APR-19 03.12.00.000000000 PM GMT
@@ -150,65 +174,75 @@ func ToTimestamp(t time.Time) *tspb.Timestamp {
 // 2019-01-01 00:00:00.0
 // 2019-01-01
 // 30.12.2018-00:00
-func (ps LoggedParseString) ParseStringToTimestamp() *tspb.Timestamp {
-	return ParseStringToTimestamp(ps.s, ps.conFailStat)
+func (lps LoggedParseString) ParseStringToTimestamp() *tspb.Timestamp {
+	return ParseStringToTimestamp(lps.s, lps.conFailStat)
 }
 
+// ParseStringToTimestamp prases the string in LoggedParseString as *tspb.Timestamp and logs any failures
 func ParseStringToTimestamp(s string, conFailStat *sync.Map) *tspb.Timestamp {
 
 	return ToTimestamp(ParseStringToTime(s, conFailStat))
 }
 
+// stringRemoveTZAbrevation removes Timezone Abbreviations seperated by " " like GMT from the string in LoggedParseString and logs any failures
+// returns erro if there is no Timezone defined
 func (lps LoggedParseString) stringRemoveTZAbrevation() (string, error) {
 	var err error
 	substrings := strings.Split(lps.s, " ")
-	substings_len := len(substrings)
-	tz_suffix := substrings[substings_len-1]
-	_, err = time.LoadLocation(tz_suffix)
+	substingsLen := len(substrings)
+	tzSuffix := substrings[substingsLen-1]
+	_, err = time.LoadLocation(tzSuffix)
 	if err != nil {
-		storeFailure("'"+lps.s+"' could not remove "+tz_suffix+" as TimeZone Abbreviation GMT", lps.conFailStat)
+		storeFailure("'"+lps.s+"' could not remove "+tzSuffix+" as TimeZone Abbreviation GMT", lps.conFailStat)
 		return "", err
 	}
-	s_prefix := strings.Join(substrings[:substings_len-1], " ")
-	s_prefix = strings.TrimSpace(s_prefix)
-	return s_prefix, err
+	sPrefix := strings.Join(substrings[:substingsLen-1], " ")
+	sPrefix = strings.TrimSpace(sPrefix)
+	return sPrefix, err
 }
 
-func (ps LoggedParseString) stringRemoveTZOffset(tz_suffix_layout string) (string, error) {
-	return stringRemoveTZOffset(ps, tz_suffix_layout)
-}
-
+// stringRemoveTZOffset removes Timezone Offsets like -07 -0700 -07:00 from the string in LoggedParseString and logs any failures
 // Cuts tz_suffix = '+/-07:00' || '+/-0700' || +/-07 from prefix+07:00 (7 exemplary)
-func stringRemoveTZOffset(ps LoggedParseString, tz_suffix_layout string) (string, error) {
+// returns erro if the cut part is short or longer than the tzSuffixLayout defined
+func (lps LoggedParseString) stringRemoveTZOffset(tzSuffixLayout string) (string, error) {
+	return stringRemoveTZOffset(lps, tzSuffixLayout)
+}
+
+// stringRemoveTZOffset removes Timezone Offsets like -07 -0700 -07:00 from the string s and logs any failures in conFailStat
+// Cuts tz_suffix = '+/-07:00' || '+/-0700' || +/-07 from prefix+07:00 (7 exemplary)
+// returns erro if the cut part is short or longer than the tzSuffixLayout defined
+func stringRemoveTZOffset(ps LoggedParseString, tzSuffixLayout string) (string, error) {
 	var err error
-	s_len := len(ps.s)
+	sLength := len(ps.s)
 	var splitsPlus, splitMinus []string
 
 	splitsPlus = strings.Split(ps.s, "+")
-	s_prefix := splitsPlus[0]
+	sPrefix := splitsPlus[0]
 
 	if len(splitsPlus) <= 1 {
 		splitMinus = strings.Split(ps.s, "-")
-		splitMinus_len := len(splitMinus)
-		s_prefix = strings.Join(splitMinus[:splitMinus_len-1], "-")
-		if len(splitMinus[splitMinus_len-1]) < len(tz_suffix_layout)-1 {
-			err = errors.New("TZ Suffix has not the Format " + tz_suffix_layout)
+		splitMinusLength := len(splitMinus)
+		sPrefix = strings.Join(splitMinus[:splitMinusLength-1], "-")
+		if len(splitMinus[splitMinusLength-1]) < len(tzSuffixLayout)-1 {
+			err = errors.New("TZ Suffix has not the Format " + tzSuffixLayout)
 		}
-	} else if len(splitsPlus[1]) < len(tz_suffix_layout)-1 {
-		err = errors.New("TZ Suffix has not the Format " + tz_suffix_layout)
+	} else if len(splitsPlus[1]) < len(tzSuffixLayout)-1 {
+		err = errors.New("TZ Suffix has not the Format " + tzSuffixLayout)
 	}
 
-	if err != nil || len(s_prefix)+len(tz_suffix_layout) != s_len {
-		storeFailure("'"+ps.s+"' could not remove TimeZone with Format "+tz_suffix_layout, ps.conFailStat)
+	if err != nil || len(sPrefix)+len(tzSuffixLayout) != sLength {
+		storeFailure("'"+ps.s+"' could not remove TimeZone with Format "+tzSuffixLayout, ps.conFailStat)
 	}
-	s_prefix = strings.TrimSpace(s_prefix)
-	return s_prefix, err
+	sPrefix = strings.TrimSpace(sPrefix)
+	return sPrefix, err
 }
 
-func (ps LoggedParseString) ParseStringToDate() *tspb.Timestamp {
-	return ParseStringToDate(ps.s, ps.conFailStat)
+//ParseStringToDate !Depricated! removes the timezone offset before parsing the string from LoggedParseString as *tspb.Timestamp and logging any failures
+func (lps LoggedParseString) ParseStringToDate() *tspb.Timestamp {
+	return ParseStringToDate(lps.s, lps.conFailStat)
 }
 
+//ParseStringToDate !Depricated! removes the timezone offset before parsing the string s as *tspb.Timestamp and logging any failures to conFailStat
 func ParseStringToDate(s string, conFailStat *sync.Map) *tspb.Timestamp {
 	stringTZFree, err := stringRemoveTZOffset(LoggedParseString{s, conFailStat}, "-07:00")
 	if err != nil {
@@ -218,31 +252,33 @@ func ParseStringToDate(s string, conFailStat *sync.Map) *tspb.Timestamp {
 	return ToTimestamp(ParseStringToTime(stringTZFree, conFailStat))
 }
 
-func (lps LoggedParseString) removeTZ(p Pair) (string, error) {
+func (lps LoggedParseString) removeTZ(p TimeLayoutTrimedTimeZone) (string, error) {
 	var err error
-	var s_prefix string
-	tz_suffix_layout, ok := p[1].(string)
-	if ok {
-		switch tz_suffix_layout {
-		case "GMT":
-			s_prefix, err = lps.stringRemoveTZAbrevation()
-		case "-0700", "-07:00", "-07":
-			s_prefix, err = lps.stringRemoveTZOffset(tz_suffix_layout)
-		default:
-			err = errors.New("Can not Remove TZ")
-		}
-		//TODO
-		return s_prefix, err
-	}
+	var sPrefix string
+	tzSuffixLayout := p.tZoneLayoutToRemove
 
-	return "", errors.New("TZ Layout needs to be string")
+	switch tzSuffixLayout {
+	case "GMT":
+		sPrefix, err = lps.stringRemoveTZAbrevation()
+	case "-0700", "-07:00", "-07":
+		sPrefix, err = lps.stringRemoveTZOffset(tzSuffixLayout)
+	default:
+		err = errors.New("Can not Remove TZ")
+		sPrefix = ""
+	}
+	return sPrefix, err
 }
 
-func TryLayoutsToParseStringToTimeWithoutTZ(lps LoggedParseString, importLayoutsWithoutTimeZone []Pair) (time.Time, error) {
+//TryLayoutsToParseStringToTimeWithoutTZ trys to parse the string within LoggedParseString to time.Time without Timezone information using the importLayoutsWithoutTimeZone []Pair
+// []Pair should contain Pair{"TimeLayoutwithoutTimeZone","TimeZoneLayoutToRemove"}
+// example Pair{"2006-01-02", "-07:00"}
+func TryLayoutsToParseStringToTimeWithoutTZ(lps LoggedParseString, importLayoutsWithoutTimeZone []TimeLayoutTrimedTimeZone) (time.Time, error) {
+	localtime, _ := time.LoadLocation("Europe/Berlin")
+	time.Local = localtime
 	for _, importLayoutPair := range importLayoutsWithoutTimeZone {
-		s_TZFree, tz_err := lps.removeTZ(importLayoutPair)
-		if tz_err == nil {
-			newTime, err := time.Parse(importLayoutPair[0].(string), s_TZFree)
+		stringTZFree, tzError := lps.removeTZ(importLayoutPair)
+		if tzError == nil {
+			newTime, err := time.Parse(importLayoutPair.tLayoutPostTZRemoval, stringTZFree)
 			if err == nil {
 				return newTime, err
 			}
@@ -252,7 +288,12 @@ func TryLayoutsToParseStringToTimeWithoutTZ(lps LoggedParseString, importLayouts
 	return time.Time{}, errors.New("Could not Parse without TZ with this ImportLayouts")
 }
 
-func TryLayoutsToParseStringToTime(lps LoggedParseString, importLayouts []string) (time.Time, error) {
+//TryLayoutsToParseStringToTimeWithTZ trys to parse the string within LoggedParseString to time.Time using the importLayouts []string
+// for layout information: https://yourbasic.org/golang/format-parse-string-time-date-example/
+// example importLayouts := []string{"02.01.2006-03:04","02.01.2006-15:04","2006-01-02","2006-01-02-07:00","2006-01-02T15:04:05.999999-07:00",}
+func TryLayoutsToParseStringToTimeWithTZ(lps LoggedParseString, importLayouts []string) (time.Time, error) {
+	localtime, _ := time.LoadLocation("Europe/Berlin")
+	time.Local = localtime
 	for _, importLayout := range importLayouts {
 		newTime, err := time.Parse(importLayout, lps.s)
 
@@ -265,24 +306,32 @@ func TryLayoutsToParseStringToTime(lps LoggedParseString, importLayouts []string
 	return time.Time{}, errors.New("Could not Parse with this ImportLayouts")
 }
 
+//TryLayoutsToParseStringToTime decides depending on the parameter to trys to parse the string within LoggedParseString
+// to time.Time with or without timezone information
+// With tz: []string{"02.01.2006-03:04","02.01.2006-15:04","2006-01-02","2006-01-02-07:00","2006-01-02T15:04:05.999999-07:00",}
+// Without tz: []Pair{Pair{"2006-01-02", "-07:00"},	Pair{"15:04:05", "-07:00"},	}
 func (lps LoggedParseString) TryLayoutsToParseStringToTime(i interface{}) (time.Time, error) {
 	var newTime time.Time
 	var err error
 	switch v := i.(type) {
 	case []string:
-		newTime, err = TryLayoutsToParseStringToTime(lps, v)
+		newTime, err = TryLayoutsToParseStringToTimeWithTZ(lps, v)
 		return newTime, err
-	case []Pair:
+	case []TimeLayoutTrimedTimeZone:
 		newTime, err = TryLayoutsToParseStringToTimeWithoutTZ(lps, v)
 		return newTime, err
 	default:
-		err = errors.New("Needs []string or []Pair as Parameter")
+		err = errors.New("Needs []string or []TimeLayoutTrimedTimeZone as Parameter")
 	}
 	return time.Time{}, err
 }
 
+// Pair used as p := Pair{"2006-01-02", "-07:00"} to define the tz format p[1]
+// which shall be removed from a string so that the resulting string has the format p[0]
+// so that string can be parsed as time
 type Pair [2]interface{}
 
+//ParseStringToTime trys to parse a string as time.Time and logs failures in conFailStat
 // 01-APR-19 03.12.00.000000000 PM +02:00
 // 01-APR-19 03.12.00 PM +02:00
 // 01-APR-19 03.12.00.000000000 PM GMT
@@ -291,7 +340,10 @@ type Pair [2]interface{}
 // 2019-01-01
 // 30.12.2018-00:00
 func ParseStringToTime(s string, conFailStat *sync.Map) time.Time {
-	localtime, _ := time.LoadLocation("Europe/Berlin")
+	localtime, locationError := time.LoadLocation("Europe/Berlin")
+	if locationError != nil {
+		logrus.Debug(locationError)
+	}
 	time.Local = localtime
 	importLayouts := []string{
 		// for layout information: https://yourbasic.org/golang/format-parse-string-time-date-example/
@@ -301,7 +353,7 @@ func ParseStringToTime(s string, conFailStat *sync.Map) time.Time {
 		"02.01.2006-03:04",
 		"02.01.2006-15:04",
 		"2006-01-02",
-		"2006-01-02-07:00",
+		// "2006-01-02-07:00",
 		"2006-01-02T15:04:05.999999-07:00",
 		"2006-01-02 15:04:05.999",
 		"2006-01-02 03:04:05.999",
@@ -320,10 +372,10 @@ func ParseStringToTime(s string, conFailStat *sync.Map) time.Time {
 		"20060102 30405",
 		"20060102 304",
 	}
-	importLayoutsWithoutTimeZone := []Pair{
-		Pair{"2006-01-02", "-07:00"},
-		Pair{"15:04:05", "-07:00"},
-	}
+	// importLayoutsWithoutTimeZone := []Pair{
+	// 	Pair{"2006-01-02", "-07:00"},
+	// 	Pair{"15:04:05", "-07:00"},
+	// }
 
 	newTime := time.Time{}
 	loggedParseString := LoggedParseString{s, conFailStat}
@@ -335,17 +387,5 @@ func ParseStringToTime(s string, conFailStat *sync.Map) time.Time {
 			storeFailure("'"+s+"' asTime", conFailStat)
 		}
 	}
-
 	return newTime
-	/* for _, importLayout := range importLayouts {
-		newTimestamp, err := time.Parse(importLayout, s)
-
-		if err == nil {
-			// fmt.Printf("String: %s got parsed to: %v \n", s, newTimestamp)
-			return newTimestamp
-		}
-	}
-	storeFailiure("'"+s+"' asTime", conFailStat)
-	return time.Time{} */
-
 }
